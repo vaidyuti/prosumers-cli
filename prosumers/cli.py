fc95dd9243a02bf1097bcfd799a7a344991089fc
Rithvik Nishad <rithvikn2001@gmail.com>
2022-12-06 10:53:12 +0530

    minor refactors

--- prosumers/cli.py
+++ prosumers/cli.py
@@ -2,13 +2,33 @@ import click
 from click import echo, style
 
 from .config import inspect_config, list_config_envs, load_config
-from .scenario import read_scenario
+from . import commands
 
 
 @click.group()
 def cli():
     """A CLI tool to emulate prosumers for testing the DEX platform."""
-    pass
+
+
+@click.command()
+def list_configs():
+    """Lists all configured environments."""
+    commands.list_configs()
+
+
+@click.command()
+@click.argument("env", default="local", required=False)
+@click.option("--all", "-a", is_flag=True, help="Show all configurations.")
+def config(env, all):
+    """Show/edit the configurations of various environments."""
+    commands.config()
+
+
+@click.command()
+@click.argument("scenario", default="scenario.yaml", required=False)
+def verify(scenario):
+    """Verifies the integrity of the scenario file."""
+    commands.verify(scenario)
 
 
 @click.command()
@@ -17,54 +37,7 @@ def cli():
 @click.option("--verbose", "-v", is_flag=True, help="Run in verbose mode.")
 def run(scenario, env, verbose):
     """Runs the emulation with the scenario file."""
-    echo("Verifying '{0}'".format(scenario))
-
-
-@click.command()
-@click.argument("scenario", default="scenario.yaml", required=False)
-def verify(scenario):
-    """Verifies the integrity of the scenario file."""
-    echo("Verifying '{0}'...".format(scenario))
-    try:
-        obj = read_scenario(scenario)
-        echo(click.style(f"Verified.", fg="bright_green", bold=True))
-    except Exception as ex:
-        echo(click.style(ex, fg="red", bold=True))
-
-
-@click.command()
-@click.argument("env", default="local", required=False)
-@click.option("--all", "-a", is_flag=True, help="Show all configurations.")
-def config(env, all):
-    """Show/edit the configurations of various environments."""
-    if all:
-        config = load_config()
-        if config != None:
-            envs = config.values()
-            echo(style("{0} environments found.\n".format(len(envs)), fg="black"))
-            div = style("-" * 64, fg="black")
-            echo(div)
-            for env in envs:
-                env.detail()
-                echo(div)
-    else:
-        env_config = inspect_config(env)
-        if env_config:
-            env_config.detail()
-        else:
-            echo(style("No config for '{0}' env was found.".format(env), fg="yellow"))
-
-
-@click.command()
-def list_configs():
-    """Lists all configured environments."""
-    envs = list_config_envs()
-    if envs:
-        echo(style("{0} environments found.".format(len(envs)), fg="black"))
-        for env in envs:
-            echo(env)
-    else:
-        echo(style("No environments configured.", fg="black"))
+    commands.verify(scenario)
 
 
 cli.add_command(run)
---
+++ prosumers/commands.py
@@ -0,0 +1,43 @@
+from click import echo, style
+import click
+
+from .config import inspect_config, list_config_envs, load_config
+from .scenario import read_scenario
+
+
+def verify(scenario):
+    echo("Verifying '{0}'...".format(scenario))
+    try:
+        obj = read_scenario(scenario)
+        echo(click.style(f"Verified.", fg="bright_green", bold=True))
+    except Exception as ex:
+        echo(click.style(ex, fg="red", bold=True))
+
+
+def config(env, all):
+    if all:
+        config = load_config()
+        if config != None:
+            envs = config.values()
+            echo(style("{0} environments found.\n".format(len(envs)), fg="black"))
+            div = style("-" * 64, fg="black")
+            echo(div)
+            for env in envs:
+                env.detail()
+                echo(div)
+    else:
+        env_config = inspect_config(env)
+        if env_config:
+            env_config.detail()
+        else:
+            echo(style("No config for '{0}' env was found.".format(env), fg="yellow"))
+
+
+def list_configs():
+    envs = list_config_envs()
+    if envs:
+        echo(style("{0} environments found.".format(len(envs)), fg="black"))
+        for env in envs:
+            echo(env)
+    else:
+        echo(style("No environments configured.", fg="black"))
